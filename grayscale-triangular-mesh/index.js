fd3f59199e509711252b47a4edd350febaa70df8
Adam Morris <aimorris@aimorris.com>
2020-08-05 18:17:12 +1000

    offset dots

--- grayscale-triangular-mesh/index.js
+++ grayscale-triangular-mesh/index.js
@@ -1,18 +1,25 @@
-let gap = innerWidth / 10;
-
+let gap = innerWidth / 19;
 let points = [];
+let offset = false;
 
 function setup() {
   canvas = createCanvas(innerWidth, innerHeight);
   
   for (let i = gap / 2; i < innerWidth; i += gap) {
     for (let j = gap / 2; j < innerHeight; j += gap) {
-      points.push(
-        {
+      if (offset) {
+        points.push({
+          x: i + gap,
+          y: j
+        })
+      } else {
+        points.push({
           x: i,
           y: j
-        }
-      )
+        })
+      }
+
+      offset = !offset;
     }
   }
 }
