461fd42bb04e135cd6e7ff554188dee7aa86387b
Adam Morris <aimorris@aimorris.com>
2020-08-05 18:13:58 +1000

    dot array

--- grayscale-triangular-mesh/index.js
+++ grayscale-triangular-mesh/index.js
@@ -1,7 +1,30 @@
-function setup() {
+let gap = innerWidth / 10;
 
+let points = [];
+
+function setup() {
+  canvas = createCanvas(innerWidth, innerHeight);
+  
+  for (let i = gap / 2; i < innerWidth; i += gap) {
+    for (let j = gap / 2; j < innerHeight; j += gap) {
+      points.push(
+        {
+          x: i,
+          y: j
+        }
+      )
+    }
+  }
 }
 
 function draw() {
-  
+  background(33);
+
+  stroke(255);
+  strokeWeight(3);
+  noFill();
+
+  for (let i = 0; i < points.length; i++) {
+    point(points[i].x, points[i].y);
+  }
 }
