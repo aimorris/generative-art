b1ff7ad0afb75f5f7c106895d1393ce7e3ce45f1
Adam Morris <aimorris@aimorris.com>
2020-08-05 18:57:58 +1000

    done?

--- grayscale-triangular-mesh/index.js
+++ grayscale-triangular-mesh/index.js
@@ -1,37 +1,40 @@
-let gap = innerWidth / 19;
+let gapX = innerWidth / 19;
+let gapY = innerHeight / 39;
 let points = [];
-let offset = false;
+let offset = true;
+let canvas;
 
 function setup() {
   canvas = createCanvas(innerWidth, innerHeight);
-  
-  for (let i = gap / 2; i < innerWidth; i += gap) {
-    for (let j = gap / 2; j < innerHeight; j += gap) {
+
+  for (let i = gapX / 2; i < innerWidth - gapX; i += gapX) {
+    let line = [];
+    for (let j = gapY / 2; j < innerHeight; j += gapY) {
+      let randomX = (Math.random() * 10 - 5) * gapX + (offset ? gapX / 2 : 0);
+      let randomY = (Math.random() * 10 - 5) * gapY;
       if (offset) {
-        points.push({
-          x: i + gap,
-          y: j
+        line.push({
+          x: i + gapX + randomX,
+          y: j + randomY
         })
       } else {
-        points.push({
-          x: i,
-          y: j
+        line.push({
+          x: i + randomX,
+          y: j + randomY
         })
       }
 
       offset = !offset;
     }
+    for (let j = 0; j < line.length - 2; j++) {
+      let gray = Math.floor(Math.random()*16).toString(16);
+      fill('#' + gray + gray + gray);
+      drawTriangle(line[j], line[j+1], line[j+2]);
+    }
+    points.push(line);
   }
 }
 
-function draw() {
-  background(33);
-
-  stroke(255);
-  strokeWeight(3);
-  noFill();
-
-  for (let i = 0; i < points.length; i++) {
-    point(points[i].x, points[i].y);
-  }
+function drawTriangle(a, b, c) {
+  triangle(a.x, a.y, b.x, b.y, c.x, c.y);
 }
